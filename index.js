b254fd10d5c13e694e99ec735197910f4106f5ee
Etel Sverdlov <beigeotter@gmail.com>
2019-01-07 11:44:31 +0100

    words are objects, console log gets delayed.

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const request = require("request");
 const key =
   "dict.1.1.20190101T143458Z.9e934ebb5eb8f106.4c671e8c7f3a082535985d61affef702184348ad";
 var troubledWords = [];
+var vocabList = [];
+
+class Word {
+  constructor(
+    germanWord,
+    partOfSpeech,
+    wordEndings,
+    wordGender,
+    englishDefinition
+  ) {
+    this.germanWord = germanWord;
+    this.partOfSpeech = partOfSpeech;
+    this.wordEndings = wordEndings;
+    this.wordGender = wordGender;
+  }
+}
 
 function translateWord(wordSought) {
   request(
@@ -27,9 +43,17 @@ function translateWord(wordSought) {
         // //console.log("plural= " + wordPlural);
         // console.log("definition = " + englishDefinition);
         // console.log("");
+        var aword = new Word(
+          germanWord,
+          partOfSpeech,
+          wordEndings,
+          wordGender,
+          englishDefinition
+        );
+        vocabList.push(aword);
       } catch (err) {
         troubledWords.push(wordSought);
-        console.log(troubledWords);
+
         //return troubledWords;
       }
     }
@@ -70,10 +94,23 @@ function arrangeWordsForTranslation(wordsToTranslate) {
   createVocabList(sorted);
 }
 
-//translateWord("kalender");
+function displayInterestingThings() {
+  console.log(troubledWords);
+  console.log(vocabList);
+}
+
+Word.sort(function(a, b){
+  var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
+  if (nameA < nameB) //sort string ascending
+      return -1 
+  if (nameA > nameB)
+      return 1
+  return 0 //default return value (no sorting)
+
 arrangeWordsForTranslation(
   `Die Tochter eines reichen Mannes wächst wohlbehütet auf. Als die Mutter stirbt, bittet sie auf dem Totenbett die Tochter, ein Bäumlein auf ihrem Grab zu pflanzen, an dem sie rütteln solle, wenn sie einen Wunsch habe, was die Tochter auch tut. Zwei Jahre nach dem Tod ihrer Mutter heiratet der Vater eine Witwe, die zwei Töchter mit ins Haus bringt.`
 );
+setTimeout(displayInterestingThings, 20000);
 
 //good code relating to errors:
 
@@ -83,3 +120,5 @@ arrangeWordsForTranslation(
 //           `https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${key}&lang=de-en&text=${wordSought}`
 //         );
 //         console.log(``);
+
+// Als die Mutter stirbt, bittet sie auf dem Totenbett die Tochter, ein Bäumlein auf ihrem Grab zu pflanzen, an dem sie rütteln solle, wenn sie einen Wunsch habe, was die Tochter auch tut. Zwei Jahre nach dem Tod ihrer Mutter heiratet der Vater eine Witwe, die zwei Töchter mit ins Haus bringt.`
